c962d1d8450a78c4ce304db2303484b70db60bf0
vinay rajagopalan <vinaraja@indiana.edu>
2015-03-23 00:38:12 -0400

    Design is left

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 var express = require('express');
+var connect = require('connect');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-var app     = express();
+var url = require("url");
+//var app     = express();
+var http=require ('http');
+var links=[];
+var jpg=[];
+var flag=true;
+var app = connect()
+	.use(connect.bodyParser())
+	.use(connect.static('public'))
+	.use(function (req, res) {
 
-app.get('/scrape', function(req, res){
 	// Let's scrape Anchorman 2
-	url = 'http://www.newseum.org/todaysfrontpages/?tfp_show=100';
-    var links=[];
-	var jpg=[];
-	request(url, function(error, response, html){
+	
+	//url = 'http://www.newseum.org/todaysfrontpages/?tfp_show=100';
+    if(flag==true)
+	{
+	request("http://www.newseum.org/todaysfrontpages/?tfp_show=100", function(error, response, html){
 		if(!error){
 			var $ = cheerio.load(html);
 
@@ -34,41 +44,56 @@ app.get('/scrape', function(req, res){
 									
 												//var title, release, rating;
 												//var json = { title : "", release : "", rating : ""};
-												console.log("function called");
+												//console.log("function called");
 												var k=0;
 												$('.colorbox-1230').filter(function(){
-													console.log("function2 called");
+													//console.log("function2 called");
 													var data1 = $(this).attr('src');
 													
-													if(data1.search('.jpg')&&k==1)
-													{jpg.push(data1);}
+													if(data1.search('.jpg')&&k==1&&jpg.length<100)
+													{jpg.push('"'+data1+'"');}
 													k++;
-													/**fs.appendFile("C:/Users/vinay/Documents/GitHub/node-web-scraper-master/text.txt", data1, 				function(err) {
-																if(err) {
-																	return console.log(err);
-																}
-															
-																console.log("The txt file was saved!");
-															}); **/
+													
 											
 												})		       
 											}
-								
+									
 		
 							})
+							
+							
 				}
+				
+				
 		         
-	        })		       
+	        })
+					       
 		} 
-		
-		res.send('Check your console!')   
+			
+		flag=false;
+	
 	})
 	
-	    
+	}
+	function random(high,low) {
+    high++;
+    return Math.floor((Math.random())*(high-low))+low;
+	}
+	var parsed_url = url.parse(req.url);
+		var uri = parsed_url.pathname;
+		if(uri === "/test"){
+			var x=random(0,jpg.length);
+			console.log(x);
+	   res.writeHead(200, {"Content-Type": "text/plain"});	
+	     						
+		res.end(jpg[x]);
+		//console.log(jpg.length);
 	   
+		}
+		
 		
 })
 
-app.listen('8081')
+app.listen('3600')
 console.log('Magic happens on port 8081');
 exports = module.exports = app; 
